7fbdd9dff2999749a0169810015299b05b138e36
Jaco Kotzee <jakwakwa@gmail.com>
2025-12-10 11:26:44 +0200

    build: Update `resolve` import path and remove two alternative HTML build entry points.

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,11 @@
+import { resolve } from "node:path";
 import { defineConfig } from "vite";
-import { resolve } from "path";
 
 export default defineConfig({
 	build: {
 		rollupOptions: {
 			input: {
 				main: resolve(__dirname, "index.html"),
-				alt1: resolve(__dirname, "shares-certificate-alt001.html"),
-				alt2: resolve(__dirname, "shares-certificate-alt002.html"),
 			},
 		},
 	},
